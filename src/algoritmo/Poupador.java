f0efd3e7346b30c03063302ea2d9674552159d2d
clailsonlg <clailsonlucas@gmail.com>
2021-08-23 22:27:48 -0300

    commit 23-08-21

--- src/algoritmo/Poupador.java
+++ src/algoritmo/Poupador.java
@@ -1,16 +1,19 @@
 package algoritmo;
 
+import java.util.Arrays;
+
 public class Poupador extends ProgramaPoupador {
 	
 	public int acao() {
 		
 //		System.out.println(sensor.getVisaoIdentificacao()[11]);
 //		System.out.println(sensor.getPosicao());
+//		System.out.println("getX - " + sensor.getPosicao().getX());
+//		System.out.println("getY - " + sensor.getPosicao().getY());
 		
-		int[] peso_passo = new int[5];
-		int[] peso_campos = new int[24];
+		float[] peso_campos = new float[24];
 		
-		int[] soma_peso = new int[4];
+		float[] soma_peso = new float[4];
 		
 		for(int i = 0; i < 24; i++ ) {
 			
@@ -18,11 +21,11 @@ public class Poupador extends ProgramaPoupador {
 				sensor.getVisaoIdentificacao()[i] == -1 || 
 				sensor.getVisaoIdentificacao()[i] == 1 || 
 				sensor.getVisaoIdentificacao()[i] == 200) {
-				peso_campos[i] = 100;
+				peso_campos[i] = 200;
 			}
 			
 			if(sensor.getVisaoIdentificacao()[i] == 0) {
-				peso_campos[i] = 0;
+				peso_campos[i] = 50;
 			}
 			
 			if(sensor.getVisaoIdentificacao()[i] == 3) {
@@ -48,52 +51,49 @@ public class Poupador extends ProgramaPoupador {
 		soma_peso[2] = peso_campos[3] + peso_campos[4] + peso_campos[8] + peso_campos[9] + peso_campos[12] + peso_campos[13] + peso_campos[17] + peso_campos[18] + peso_campos[22] + peso_campos[23];
 		soma_peso[3] = peso_campos[0] + peso_campos[1] + peso_campos[5] + peso_campos[6] + peso_campos[10] + peso_campos[11] + peso_campos[14] + peso_campos[15] + peso_campos[19] + peso_campos[20];
 		
-		int[] array = new int[4];
-		int maior = 0;
+		float[] soma_peso_ordenado = new float[4];
+		int[] index = new int[4];
+		float soma_total = 0;
+
+		for(int i = 0; i < 4; i++) {
+			soma_peso_ordenado[i] = soma_peso[i];
+			soma_total = soma_total + soma_peso[i];
+		}
+		
+		Arrays.sort(soma_peso_ordenado);
 		
 		for(int i = 0; i < 4; i++) {
-			if(i == 0) {
-				maior 
+			for(int j = 0; j < 4; j++) {
+				if(soma_peso_ordenado[i] == soma_peso[j]) {
+					index[i] = j;
+				}
 			}
 		}
 		
+		double a = Math.random();
+		float total = soma_peso_ordenado[0] / soma_total;
 		
+		System.out.println(a);
+	
+		if(a > soma_peso_ordenado[0] / soma_total) {
+			System.out.println("Direção: " + (index[0] + 1));
+			return index[0] + 1;
+		} 
+		else if(a > soma_peso_ordenado[1] / soma_total) {
+			System.out.println("Direção: " + (index[0] + 1));
+			return index[1] + 1;
+		}
+		else if(a > soma_peso_ordenado[2] / soma_total) {
+			System.out.println("Direção: " + (index[0] + 1));
+			return index[2] + 1;
+		}
+		else if(a > soma_peso_ordenado[3] / soma_total) {
+			System.out.println("Direção: " + (index[0] + 1));
+			return index[3] + 1;
+		}
 		
-			
-//			System.out.println(sensor.getVisaoIdentificacao()[i]);
-//			System.out.println("getx - " + sensor.getPosicao().getX());
-			
-//			if(sensor.getVisaoIdentificacao()[i] == 4) {
-//				if (i == 0 || i == 1 || i == 5 || i == 6) {
-//					if(sensor.getVisaoIdentificacao()[11] == 1) {
-//						return 1;
-//					}
-//					return 4;
-//				}
-//				if (i == 2 || i == 7) {
-//					return 1;
-//				}
-//				if (i == 3 || i == 4 || i == 8 || i == 9) {
-//					return 3;
-//				}
-//				if (i == 12 || i == 13) {
-//					return 3;
-//				}
-//				if (i == 17 || i == 18 || i == 22 || i == 23) {
-//					return 2;
-//				}
-//				if (i == 16 || i == 21) {
-//					return 2;
-//				}
-//				if (i == 14 || i == 15 || i == 19 || i == 20) {
-//					return 2;
-//				}
-//				if (i == 10 || i == 11) {
-//					return 4;
-//				} 
-//			}
-			
-//		return (int) (Math.random() * 5);
+		return (int) (Math.random() * 5);
+		
 	}
 
 }
