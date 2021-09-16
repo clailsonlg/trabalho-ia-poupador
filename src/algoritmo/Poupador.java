a39a5d9385369fc6776813224e34217bd4ef3f32
clailsonlg <clailsonlucas@gmail.com>
2021-09-15 21:49:55 -0300

    15-09-21

--- src/algoritmo/Poupador.java
+++ src/algoritmo/Poupador.java
@@ -1,6 +1,7 @@
 package algoritmo;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Poupador extends ProgramaPoupador {
 	
@@ -8,42 +9,76 @@ public class Poupador extends ProgramaPoupador {
 		
 //		System.out.println(sensor.getVisaoIdentificacao()[11]);
 //		System.out.println(sensor.getPosicao());
-//		System.out.println("getX - " + sensor.getPosicao().getX());
-//		System.out.println("getY - " + sensor.getPosicao().getY());
+		System.out.println("getX - " + sensor.getPosicao().getX());
+		System.out.println("getY - " + sensor.getPosicao().getY());
 		
 		float[] peso_campos = new float[24];
 		
 		float[] soma_peso = new float[4];
 		
+//		String saida = "";
+//		int[][] quadradosDoCampo = new int [30][30];
+
+		
+//		for (int l = 0; l < quadradosDoCampo.length; l++) {
+//			for (int c = 0; c < quadradosDoCampo[0].length; c++) {
+//				saida = saida + quadradosDoCampo[l][c] + ","; // imprime caracter a caracter
+//			}
+//			saida = saida + "\n"; // muda de linha
+//		}
+//		System.out.println(saida);
+		
+//		int cordx = (int) sensor.getPosicao().getX();
+//		int cordy = (int) sensor.getPosicao().getY();
+//		
+//		System.out.println("Cordx: " + cordx);
+//		System.out.println("Cordy: " + cordy);
+//		
+//		quadradosDoCampo[cordx][cordy] = quadradosDoCampo[cordx][cordy] + 1;
+//		
+//		for (int l = 0; l < 30; l++) {
+//			
+//			for (int c = 0; c < 30; c++) {
+//				if (l == cordx && c == cordy) {
+//					quadradosDoCampo[l][c] = 1;
+//				}
+//				saida = saida + quadradosDoCampo[l][c] + ","; // imprime caracter a caracter
+//			}
+//			saida = saida + "\n"; // muda de linha
+//		}
+//		System.out.println(saida);
+
+//		return (int) (Math.random() * 5);
+		
 		for(int i = 0; i < 24; i++ ) {
-			
+			// Parede
 			if(sensor.getVisaoIdentificacao()[i] == -2 || 
 				sensor.getVisaoIdentificacao()[i] == -1 || 
 				sensor.getVisaoIdentificacao()[i] == 1) {
-				peso_campos[i] = 100.0f;
+				peso_campos[i] = 200.0f;
 			}
-			
+			// Celula vazia
 			if(sensor.getVisaoIdentificacao()[i] == 0) {
 				peso_campos[i] = 30.0f;
 			}
-			
+			// Ladrao
 			if(sensor.getVisaoIdentificacao()[i] == 200) {
 				peso_campos[i] = sensor.getNumeroDeMoedas() * 50;
 				
 			}
-			
+			// Banco
 			if(sensor.getVisaoIdentificacao()[i] == 3) {
 				peso_campos[i] = 70 / (1 + sensor.getNumeroDeMoedas());
 			}
-			
+			// Moeda
 			if(sensor.getVisaoIdentificacao()[i] == 4) {
 				peso_campos[i] = 1.0f;
 			}
-			
+			// Pastilha de poder
 			if(sensor.getVisaoIdentificacao()[i] == 5) {
 				peso_campos[i] = 100 / (1 + sensor.getNumeroDeMoedas());
 			}
-			
+			// Poupador
 			if(sensor.getVisaoIdentificacao()[i] == 100) {
 				peso_campos[i] = 100.0f;
 			}
@@ -75,34 +110,46 @@ public class Poupador extends ProgramaPoupador {
 		}
 		
 		double a = Math.random();
-		float total = soma_peso_ordenado[0] / soma_total;
 		
-		System.out.println("Total float: "+ total);
+		
+		System.out.println("Aleatorio: "+ a);
 		System.out.println("Soma total: " + soma_total);
 		System.out.println("indece 0: " + soma_peso_ordenado[0]);
 		System.out.println("indece 1: " + soma_peso_ordenado[1]);
 		System.out.println("indece 2: " + soma_peso_ordenado[2]);
 		System.out.println("indece 3: " + soma_peso_ordenado[3]);
-		System.out.println(a);
+		
+		double soma3 = soma_peso_ordenado[3] + soma_peso_ordenado[2] + soma_peso_ordenado[1];
+		double soma2 = soma_peso_ordenado[2] + soma_peso_ordenado[1];
+		
 	
-		if(a > soma_peso_ordenado[0] / soma_total) {
-			System.out.println("Direção: " + (index[0] + 1));
-			return index[0] + 1;
+	
+		if(a > (soma3) / soma_total) {
+			double soma = (soma_peso_ordenado[3] + soma_peso_ordenado[2] + soma_peso_ordenado[1]) / soma_total;
+			System.out.println("Soma3: " + soma3);
+			System.out.println("Direção: " + (index[3] + 1));
+			return index[3] + 1;
 		} 
-		else if(a > soma_peso_ordenado[1] / soma_total) {
-			System.out.println("Direção: " + (index[0] + 1));
-			return index[1] + 1;
-		}
-		else if(a > soma_peso_ordenado[2] / soma_total) {
-			System.out.println("Direção: " + (index[0] + 1));
+		else if(a > (soma2) / soma_total) {
+			double soma = (soma_peso_ordenado[2] + soma_peso_ordenado[1]) / soma_total;
+			System.out.println("Soma2: " + soma2);
+			System.out.println("Direção: " + (index[2] + 1));
 			return index[2] + 1;
 		}
-		else if(a > soma_peso_ordenado[3] / soma_total) {
+		else if(a > soma_peso_ordenado[1] / soma_total) {
+			double soma = (soma_peso_ordenado[1]) / soma_total;
+			System.out.println("Soma1: " + soma);
+			System.out.println("Direção: " + (index[1] + 1));
+			return index[1] + 1;
+		}
+		else {
+			double soma = (soma_peso_ordenado[0]) / soma_total;
+			System.out.println("Soma0: " + soma);
 			System.out.println("Direção: " + (index[0] + 1));
-			return index[3] + 1;
+			return index[0] + 1;
 		}
 		
-		return (int) (Math.random() * 5);
+//		return (int) (Math.random() * 5);
 		
 	}
 
